c042dbdfe87714642a2da0f818d4b70865ca07f2
Divine David <123441812+Josdivdav@users.noreply.github.com>
2026-09-08 17:16:28 +0100

    Remove docstring from get_file_content function
    Removed docstring from get_file_content function.

--- lib/get_file_content.py
+++ lib/get_file_content.py
@@ -1,15 +1,6 @@
 import os
 
 def get_file_content(file_path):
-    """
-    Get the content of a file.
-
-    Args:
-        file_path (str): The path to the file.
-
-    Returns:
-        str: The content of the file.
-    """
     if not os.path.isfile(file_path):
         raise ValueError("File not found")
     
