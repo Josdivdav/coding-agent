50cae1a8784f2e504ba5b867dfe79f359bb0cfda
Divine David <123441812+Josdivdav@users.noreply.github.com>
2026-09-08 17:16:56 +0100

    Remove docstring from get_files function
    Removed docstring from get_files function.

--- lib/get_files.py
+++ lib/get_files.py
@@ -2,13 +2,4 @@ import os
 
 
 def get_files(directory):
-    """
-    Get a list of files in the specified directory.
-
-    Args:
-        directory (str): The path to the directory.
-
-    Returns:
-        list: A list of files in the directory.
-    """
     return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
